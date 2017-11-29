ca49b1ca9727f41c5031ddab9fbaeb7f4abba793
damien.danglard@outlook.fr <D.damiendu63>
2017-11-29 12:49:57 +0100

    create MainActivity class

---
+++ app/src/main/java/com/kassylab/callrecorder/activity/MainActivity.java
@@ -0,0 +1,265 @@
+/*
+ * Copyright (C) 2017  KassyLab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kassylab.callrecorder.activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.kassylab.callrecorder.CallFragment;
+import com.kassylab.callrecorder.R;
+import com.kassylab.callrecorder.dummy.DummyContent;
+import com.kassylab.callrecorder.fragment.ContactsFragment;
+
+public class MainActivity extends AppCompatActivity
+		implements NavigationView.OnNavigationItemSelectedListener,
+		CallFragment.OnListFragmentInteractionListener,
+		ContactsFragment.OnContactSelectedListener {
+	
+	private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 123;
+	
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link FragmentPagerAdapter} derivative, which will keep every
+	 * loaded fragment in memory. If this becomes too memory intensive, it
+	 * may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	private SectionsPagerAdapter mSectionsPagerAdapter;
+	
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	private ViewPager mViewPager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the activity.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+		
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = findViewById(R.id.container);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+		
+		TabLayout tabLayout = findViewById(R.id.tabs);
+		tabLayout.setupWithViewPager(mViewPager);
+		
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+		
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
+		
+		NavigationView navigationView = findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
+		
+		if (ContextCompat.checkSelfPermission(this,
+				Manifest.permission.READ_CONTACTS)
+				!= PackageManager.PERMISSION_GRANTED) {
+			
+			// Should we show an explanation?
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+					Manifest.permission.READ_CONTACTS)) {
+				
+				// Show an explanation to the user *asynchronously* -- don't block
+				// this thread waiting for the user's response! After the user
+				// sees the explanation, try again to request the permission.
+				
+			} else {
+				
+				// No explanation needed, we can request the permission.
+				
+				ActivityCompat.requestPermissions(this,
+						new String[]{Manifest.permission.READ_CONTACTS},
+						PERMISSIONS_REQUEST_READ_CONTACTS);
+				
+				// MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+				// app-defined int constant. The callback method gets the
+				// result of the request.
+			}
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@SuppressWarnings("StatementWithEmptyBody")
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+		// Handle navigation view item clicks here.
+		int id = item.getItemId();
+		
+		if (id == R.id.nav_camera) {
+			// Handle the camera action
+		} else if (id == R.id.nav_gallery) {
+		
+		} else if (id == R.id.nav_slideshow) {
+		
+		} else if (id == R.id.nav_manage) {
+		
+		} else if (id == R.id.nav_share) {
+		
+		} else if (id == R.id.nav_send) {
+		
+		}
+		
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		drawer.closeDrawer(GravityCompat.START);
+		return true;
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+	                                       @NonNull int[] grantResults) {
+		switch (requestCode) {
+			case PERMISSIONS_REQUEST_READ_CONTACTS: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					
+					// permission was granted, yay! Do the
+					// contacts-related task you need to do.
+					
+				} else {
+					
+					// permission denied, boo! Disable the
+					// functionality that depends on this permission.
+				}
+				return;
+			}
+			
+			// other 'case' lines to check for other
+			// permissions this app might request
+		}
+	}
+	
+	@Override
+	public void onListFragmentInteraction(DummyContent.DummyItem item) {
+	
+	}
+	
+	@Override
+	public void onContactSelected(Uri itemUri, int adapterPosition) {
+	
+	}
+	
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	private class SectionsPagerAdapter extends FragmentPagerAdapter {
+		
+		SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+		
+		@Override
+		public Fragment getItem(int position) {
+			switch (position) {
+				case 0:
+				case 1:
+					return CallFragment.newInstance();
+				case 2:
+					return ContactsFragment.newInstance();
+			}
+			return null;
+		}
+		
+		@Override
+		public int getCount() {
+			return 3;
+		}
+		
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+				case 0:
+					return getString(R.string.title_tab_all);
+				case 1:
+					return getString(R.string.title_tab_favorites);
+				case 2:
+					return getString(R.string.title_tab_contacts);
+			}
+			return null;
+		}
+	}
+}
